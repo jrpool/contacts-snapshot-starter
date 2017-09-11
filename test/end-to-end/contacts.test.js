f220c3e7e1502f3b220b79011add29ef655cb421
smithrm941 <smithrm941@gmail.com>
2017-09-11 16:21:24 -0700

    test for new contact GET route passing

--- test/end-to-end/contacts.test.js
+++ test/end-to-end/contacts.test.js
@@ -5,26 +5,41 @@ const expect = require ('chai').expect
 
 chai.use(chaiHttp);
 
-
-describe('homepage route', function(){
+context('end to end tests', function(){
   beforeEach(function() {
     execSync('npm run load_contacts')
   })
-  it('has status 200', function(done) { // <= Pass in done callback
-    chai.request('http://localhost:3000')
-    .get('/')
-    .end(function(err, res) {
-      expect(res).to.have.status(200);
-      done();                               // <= Call done to signal callback end
+
+  describe('homepage route', function(){
+
+    it('has status 200', function(done) { // <= Pass in done callback
+      chai.request('http://localhost:3000')
+      .get('/')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();                               // <= Call done to signal callback end
+      });
     });
-  });
-  it('renders index.ejs', function(done) {
-    chai.request('http://localhost:3000')
-    .get('/')
-    .end(function(err, res) {
-      expect(res.text).to.include('Jared');
-      expect(res.text).to.include('Tanner');
-      done();                               // <= Call done to signal callback end
+    it('renders index.ejs', function(done) {
+      chai.request('http://localhost:3000')
+      .get('/')
+      .end(function(err, res) {
+        expect(res.text).to.include('Jared');
+        expect(res.text).to.include('Tanner');
+        done();                               // <= Call done to signal callback end
+      });
     });
-  });
+  })
+
+  describe('new contact route', function(){
+    it('renders the new contact form', function(done) {
+      chai.request('http://localhost:3000')
+      .get('/contacts/new')
+      .end(function(err, res) {
+        expect(res.text).to.include('First name:')
+        done();
+      })
+    })
+  })
+
 })
