6fc83db81ce8c28f50bb7b1f17734a9b38e42a2a
smithrm941 <smithrm941@gmail.com>
2017-09-12 14:01:17 -0700

    submit form test passing

--- test/ui/contacts.test.js
+++ test/ui/contacts.test.js
@@ -7,7 +7,21 @@ describe(`/contacts/new`, function() {
   before(function(done) {
   browser.visit(url + '/contacts/new', done);
 });
-  it.only(`should display a contact form`, function() {
-    browser.assert.element('form[class=new-contact-form]');
-  })
+    describe('displays form', function() {
+      it(`should display a contact form`, function() {
+        browser.assert.element('form[class=new-contact-form]');
+      })
+    })
+
+    describe('submits form', function() {
+      before(function(done) {
+        browser
+          .fill('first_name', 'Rhonda')
+          .fill('last_name', 'Smith')
+          .pressButton('Add Contact', done);
+      });
+      it('should see welcome page', function() {
+        browser.assert.text('h1', 'Rhonda Smith');
+      });
+    })
 })
