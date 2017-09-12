3537e986c7852558d2950885343979dfb8401878
smithrm941 <smithrm941@gmail.com>
2017-09-12 09:51:11 -0700

    tests for findAll query passing

---
+++ test/integration/contacts.test.js
@@ -0,0 +1,26 @@
+const {execSync} = require('child_process');
+const expect = require('chai').expect
+const query = require(process.cwd() + '/src/models/contacts.js')
+
+
+context('integration tests', function() {
+  beforeEach(function() {
+    execSync('npm run load_schema && npm run load_contacts')
+  })
+
+  describe(`findAll`, () => {
+    it(`should return 3 rows`, () => {
+      return query.findAll()
+      .then(data => {
+        expect(data).to.have.lengthOf(3)
+      })
+    })
+    it(`first row should have first name Jared`, () => {
+      return query.findAll()
+      .then(data => {
+        expect(data[0].first_name).to.equal('Jared')
+      })
+    })
+  })
+
+})
