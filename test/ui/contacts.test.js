276c4df64605533a489b86db6173626b0698c1c7
smithrm941 <smithrm941@gmail.com>
2017-09-12 14:03:28 -0700

    now refreshes database before each ui test

--- test/ui/contacts.test.js
+++ test/ui/contacts.test.js
@@ -3,10 +3,15 @@ const Browser = require('zombie');
 const url = 'http://localhost:3000'
 const browser = new Browser();
 
-describe(`/contacts/new`, function() {
-  before(function(done) {
-  browser.visit(url + '/contacts/new', done);
-});
+context('ui tests', function() {
+  beforeEach(function() {
+    execSync('npm run load_schema && npm run load_contacts')
+  })
+
+  describe(`/contacts/new`, function() {
+    before(function(done) {
+      browser.visit(url + '/contacts/new', done);
+    });
     describe('displays form', function() {
       it(`should display a contact form`, function() {
         browser.assert.element('form[class=new-contact-form]');
@@ -16,12 +21,14 @@ describe(`/contacts/new`, function() {
     describe('submits form', function() {
       before(function(done) {
         browser
-          .fill('first_name', 'Rhonda')
-          .fill('last_name', 'Smith')
-          .pressButton('Add Contact', done);
+        .fill('first_name', 'Rhonda')
+        .fill('last_name', 'Smith')
+        .pressButton('Add Contact', done);
       });
-      it('should see welcome page', function() {
+      it.only('should see welcome page', function() {
         browser.assert.text('h1', 'Rhonda Smith');
       });
     })
+  })
+
 })
