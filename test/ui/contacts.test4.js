8634abb779af548a97b6e248aa4c18746f52d1f4
smithrm941 <smithrm941@gmail.com>
2017-09-13 17:46:22 -0700

    added notes for UI test quirkiness

--- test/ui/contacts.test4.js
+++ test/ui/contacts.test4.js
@@ -3,34 +3,31 @@ const Browser = require('zombie');
 const url = 'http://localhost:3000'
 let browser;
 
+//ALL BUT 3rd TEST in 6th GROUP WORK WHEN ALL TESTS RUN. SOME DON'T WORK PROPERLY IN .only:
 context.only('UI tests', function() {
   beforeEach(function(done) {
     execSync('npm run load_schema && npm run load_contacts');
     done();
   })
 
-  describe('GET /contacts/new', function() {
+//WORKS PROPERLY:
+  describe('1) GET /contacts/new', function() {
     before(function(done) {
       browser = new Browser();
       browser.visit(url + '/contacts/new', done);
-      console.log(
-        '1: confirm has keys:\n'
-        + Object.keys(browser.window.confirm).join('\n')
-      );
     });
       it('GET to /contacts/new gets new-contact form', function() {
         browser.assert.element('form[class=new-contact-form]');
-      }) // Pass and fail conditions behaving properly.
+      })
   })
 
-  describe('POST /contacts/new', function() {
+//WORKS PROPERLY:
+  describe('2) POST /contacts/new', function() {
     before(function(done) {
-      console.log('1st before');
       browser = new Browser();
       browser.visit(url + '/contacts/new', done);
     });
     before(function(done) {
-      console.log('2nd before');
       browser
       .fill('first_name', 'Rhonda')
       .fill('last_name', 'Smith')
@@ -39,9 +36,10 @@ context.only('UI tests', function() {
       it('POST of /contacts/new gets contact-detail page', function() {
         browser.assert.text('h1', 'Rhonda Smith');
       });
-    }) // Pass and fail conditions behaving properly.
+    })
 
-    describe('GET /', function() {
+//WORKS PROPERLY:
+    describe('3) GET /', function() {
       before(function(done) {
         browser = new Browser();
         browser.visit(url, done);
@@ -49,45 +47,103 @@ context.only('UI tests', function() {
       it('GET to / gets contact-list page', function() {
         browser.assert.elements('.contact-list-member', 3);
         browser.assert.elements('.delete-contact', 3);
-      }) // Pass and fail conditions behaving properly.
+      })
     })
 
-    describe('DELETE /', function() {
-      before(function(done) {
+//WORKS PROPERLY in .only. NOT WORKING when all UI tests run (does not delete first contact)
+    describe('4) yes deletion of contact from home page', function() {
+      beforeEach(function(done) {
         browser = new Browser();
         browser.visit(url, done);
       });
-      before(function(done) {
-        browser.pressButton('.delete-contact', done);
-        console.log(
-          '2: confirm has keys:\n'
-          + Object.keys(browser.window.confirm).join('\n')
-        );
-      });
-      it('1: DELETE on / gets modal confirm dialog', function(done) {
-        console.log(
-          '3: confirm has keys:\n'
-          + Object.keys(browser.window.confirm).join('\n')
-        );
+      beforeEach(function() {
         browser.on('confirm', function() {
-          console.log('Confirmation dialog displayed.');
-        });
-        done();
-        // browser.assert.elements('.delete-contact', 2);
-      }); //this one keeps passing no matter what and the actual deletion is happening
+          console.log('Confirming deletion')
+          return true
+        })
+      });
+      it('presses delete button', function() {
+        browser.pressButton('.delete-contact')
+      });
+      it('actually deletes one of the three contacts', function() {
+        browser.assert.elements('.contact-list-member', 2);
+        browser.assert.elements('.delete-contact', 2);
+      });
+      //this one keeps passing no matter what and the actual deletion is happening
       // curl -X DELETE http://localhost:3000/contacts/3 deletes 3.
       // Browser suppresses it unless confirmed.
     });
 
-    describe('individual contact page', function() {
-      before(function(done) {
+//NOT WORKING IN .only, WORKING when all UI tests are run:
+    describe('5) no deletion of contact from home page', function() {
+      beforeEach(function(done) {
+        browser = new Browser();
+        browser.visit(url, done);
+      });
+      beforeEach(function() {
+        browser.on('confirm', function() {
+          console.log('Confirming deletion')
+          return false
+        })
+      });
+      it('thought about deleting the first contact', function() {
+        browser.pressButton('.delete-contact')
+      });
+      it('returned to home page after not deleting a contact', function() {
+        browser.assert.elements('.contact-list-member', 3);
+        browser.assert.elements('.delete-contact', 3);
+      });
+      //this one keeps passing no matter what and the actual deletion is happening
+      // curl -X DELETE http://localhost:3000/contacts/3 deletes 3.
+      // Browser suppresses it unless confirmed.
+    });
+
+//NOT WORKING (see notes on third test):
+    describe('6) deleting from individual contact page', function() {
+      beforeEach(function(done) {
+        browser = new Browser();
+        browser.visit(url + '/contacts/1', done);
+      });
+      beforeEach(function() {
+        browser.on('confirm', function() {
+          console.log('Confirming deletion')
+          return true
+        })
+      });
+      it('reached Jared Grippe\'s page', function() {
+        browser.assert.text('h1', 'Jared Grippe')
+      })
+      it('deleted Jared Grippe\'s page', function() {
+        browser.pressButton('.delete-contact')
+      })
+      it('gets a 404 after deleting Jared Grippe\'s page', function() {
+        //this gives "Error: Server returned status code 404" but fails test:
+          browser.assert.status(404)
+        //this passes the test, though I expect it to fail:
+          browser.assert.text('h1', 'Jared Grippe')
+      })
+    });
+
+//NOT WORKING in .only (seems to delete contact)
+    describe('7) changed mind about deleting from individual contact page', function() {
+      beforeEach(function(done) {
         browser = new Browser();
         browser.visit(url + '/contacts/2', done);
       });
-      it('2: should ask to confirm deletion', function() {
-        browser.pressButton('.delete-contact', function() {
-          browser.prompted('Are you unsure you want to delete this contact?')
+      beforeEach(function() {
+        browser.on('confirm', function() {
+          console.log('Confirming deletion')
+          return false
         })
+      });
+      it('reached Tanner Welsh\'s page', function() {
+        browser.assert.text('h1', 'Tanner Welsh')
+      })
+      it('deleted Tanner Welsh\'s page', function() {
+        browser.pressButton('.delete-contact')
+      })
+      it('goes back to Tanner Welsh\'s page', function() {
+        browser.assert.text('h1', 'Tanner Welsh')
       })
     });
     //individual contact page:
