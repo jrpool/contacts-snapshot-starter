8fe3770bd68c4afdfd849bd7759189159ad6fdaf
Jonathan Pool <pool@stulta.com>
2017-09-14 15:00:51 -0700

    Drafted 2nd half.

--- test/ui/selenium-wd/contacts.test7.js
+++ test/ui/selenium-wd/contacts.test7.js
@@ -19,7 +19,26 @@ driver.get('http://localhost:3000/contacts/new')
   element => driver.wait(until.elementTextIs(element, 'Rhonda Smith'), 3000)
 )
 .then(() => driver.quit())
+.then(() => {
+  driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+})
+.then(() => exec('npm run load_schema && npm run load_contacts'))
+.then(() => driver.get('http://localhost:3000/contacts/new'))
+.then(() => driver.findElement(By.name('first_name')).sendKeys('Rhonda'))
+.then(() => driver.findElement(By.name('last_name')).sendKeys('Smith'))
+.then(() => driver.findElement(By.css('input[value=\'Add Contact\']')).click())
+.then(() => driver.findElement(By.css('h1')))
+.then(
+  element => driver.wait(until.elementTextIs(element, 'Rhonda Jones'), 3000)
+)
+.then(() => driver.quit())
 .catch(error => {
-  console.log(error.message);
+  console.log(
+    'Here is the error message, which should say that an '
+    + 'element with text “Rhonda Jones” could not be found:\n'
+    + error.message
+  );
   driver.quit();
 });
