dd1d1f2c291a59164f22fe7187f3ec71fb280dd6
Jonathan Pool <pool@stulta.com>
2017-09-14 16:50:08 -0700

    Finished and debugged test 2.

--- test/ui/selenium-wd/contacts.test7.js
+++ test/ui/selenium-wd/contacts.test7.js
@@ -16,29 +16,53 @@ driver.get('http://localhost:3000/contacts/new')
 .then(() => driver.findElement(By.css('input[value=\'Add Contact\']')).click())
 .then(() => driver.findElement(By.css('h1')))
 .then(
-  element => driver.wait(until.elementTextIs(element, 'Rhonda Smith'), 3000)
+  element => {
+    driver.wait(until.elementTextIs(element, 'Rhonda Smith'), 3000);
+  }
 )
+.then(() => driver.getPageSource())
+.then(source => source.includes('Rhonda&nbsp;Smith'))
+.then(answer => {
+  if (answer) {
+    return true;
+  }
+  else {
+    throw 'Rhonda Smith not found in page.';
+  }
+})
 .then(() => driver.quit())
+.then(() => exec('npm run load_schema && npm run load_contacts'))
 .then(() => {
   driver = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
 })
-.then(() => exec('npm run load_schema && npm run load_contacts'))
 .then(() => driver.get('http://localhost:3000/contacts/new'))
 .then(() => driver.findElement(By.name('first_name')).sendKeys('Rhonda'))
 .then(() => driver.findElement(By.name('last_name')).sendKeys('Smith'))
 .then(() => driver.findElement(By.css('input[value=\'Add Contact\']')).click())
 .then(() => driver.findElement(By.css('h1')))
 .then(
-  element => driver.wait(until.elementTextIs(element, 'Rhonda Jones'), 3000)
+  element => {
+    driver.wait(until.elementTextIs(element, 'Rhonda Smith'), 3000);
+  }
 )
+.then(() => driver.getPageSource())
+.then(source => source.includes('Rhonda&nbsp;Jones'))
+.then(answer => {
+  if (answer) {
+    return true;
+  }
+  else {
+    throw 'Rhonda Jones not found in page.';
+  }
+})
 .then(() => driver.quit())
 .catch(error => {
   console.log(
     'Here is the error message, which should say that an '
-    + 'element with text “Rhonda Jones” could not be found:\n'
-    + error.message
+    + '“Rhonda Jones” was not found in the page:\n'
+    + error
   );
   driver.quit();
 });
